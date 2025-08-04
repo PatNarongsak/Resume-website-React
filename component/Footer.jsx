edf7d857948d9ed767002b4e3d281ce255dd66bd
sungkung103 <narongsak_pat@hotmail.com>
2025-08-04 21:56:53 +0700

    Rework JSON and Live Demo

---
+++ component/Footer.jsx
@@ -0,0 +1,28 @@
+
+import React from "react";
+import './css/Footer.css';
+
+export default function Footer() {
+  return (
+   <footer>
+        <div class="footer-container">
+            <div class="footer-left">
+                <div>© 2025 by Narongsak Mueandao. All rights reserved.</div>
+                
+            </div>
+            <div class="footer-right">
+                <div class="footer-contact">
+                    <div><b>Call</b><br/>097-2104212</div>
+                    <div><b>Email</b><br/>narongsak_pat@hotmail.com</div>
+                    <div><b>Follow</b><br/>
+                        <a href="https://www.facebook.com/narongsak.muandao/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+  );
+}
---
+++ src/Project.jsx
@@ -0,0 +1,9 @@
+import './css/projects.css';
+
+export default function Projects() {
+    return (
+<div className="main-container-project">
+        <h1>My Projects</h1>
+</div>
+      );
+}
