3c38ab4a227ffc5235e2c6434b3482cb3d2ee35f
sungkung103 <narongsak_pat@hotmail.com>
2025-08-07 00:37:55 +0700

    Update social media links in Footer and About components

--- component/Footer.jsx
+++ component/Footer.jsx
@@ -17,7 +17,7 @@ export default function Footer() {
                     <div><b>Follow</b><br/>
                         <a href="https://www.facebook.com/narongsak.muandao/" target="_blank"><i class="fab fa-facebook-f"></i></a>
                         <a href="#"><i class="fab fa-twitter"></i></a>
-                        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                        <a href="https://github.com/PatNarongsak" target="_blank"><i class="fab fa-github"></i></a>
                         <a href="#"><i class="fab fa-instagram"></i></a>
                     </div>
                 </div>
